5243964870d8d649211cd7a1afd9b7e194a23ad5
Colin <colin.bernet@cern.ch>
2016-04-07 13:37:25 +0200

    more doc

--- tdrstyle.py
+++ tdrstyle.py
@@ -1,3 +1,18 @@
+'''
+CMS official style
+To get test plots, just do:
+ 
+    python -i test_tdrstyle.py
+
+To set up the plotting style do the following in your python macro:
+
+    import tdrstyle 
+    
+To print the CMS luminosity information, use the tdrstyle.cmsPrel function. For more information, do 
+
+    help(tdrstyle.cmsPrel)
+'''
+
 import ROOT as rt
 
 
